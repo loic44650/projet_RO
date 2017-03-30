cc8cd6ba6e208216d59f41321511186629c10ab9
Divinity <divinity@archlinux.localdomain>
2017-03-30 09:06:35 +0200

    modif makefile & dossier

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
-all:
-	g++ -std=c++11 main.cpp -c -g
+all: 
+	g++ -std=c++11 src/main.cpp -Ofast -Wall -c -g
 	g++ -std=c++11 main.o -lglpk -lm -o main.exe
+
+clean:
+	rm -rf *.o *.exe
--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,7 @@ struct donnees
 
 #include "fonction_lecture.cpp"
 #include "fonctions_tries_ensembles.cpp"
-//#include "fonction_glpk.cpp"
+#include "fonction_glpk.cpp"
 
 
 /* structures et fonctions de mesure du temps CPU */
